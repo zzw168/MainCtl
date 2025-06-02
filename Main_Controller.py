f0489c9e9a1f1563f420a996da94b0f78eae3f5b
zzw168 <78839407@qq.com>
2025-06-02 08:22:39 +0400

    z

--- Main_Controller.py
+++ Main_Controller.py
@@ -4013,7 +4013,7 @@ def plan_refresh():  # 刷新方案列表
             num = ui.comboBox_plan.currentIndex() + 1  # 方案索引+1
             audio_points[index][0].move(*audio_points[index][num][1])  # 设置初始位置
             # audio_points[index][0].show()
-    if camera_points:
+    if ai_points:
         for index in range(len(ai_points)):  # 卫星图刷新
             num = ui.comboBox_plan.currentIndex() + 1  # 方案索引+1
             ai_points[index][0].move(*ai_points[index][num][1])  # 设置初始位置
@@ -7511,6 +7511,11 @@ if __name__ == '__main__':
     flg_start = {'card': False, 's485': False, 'obs': False, 'live': False,
                  'ai': False, 'ai_end': False, 'server': False}  # 各硬件启动标志
 
+    camera_points = []  # 摄像机移动点位 camera_points[[label内存],[区域号],[卫星图坐标]]
+    audio_points = []  # 音效点位 audio_points[[label内存],[区域号],[卫星图坐标]]
+    ai_points = []  # AI点位 ai_points[[label内存],[区域号],[卫星图坐标]]
+    load_plan_json()
+
     tb_step_worker = UiWorker(ui.tableWidget_Step)
     # main_music_worker = UiWorker(ui.checkBox_main_music)
     alarm_worker = UiWorker(ui.checkBox_alarm)
@@ -7860,9 +7865,9 @@ if __name__ == '__main__':
     # 开奖记录 lottery_term[期号, 开跑时间, 倒数, 状态, 自动赛果, 确认赛果, 发送状态,
     #                       图片上传状态, 备注, 图片, 录像, 结束时间, 数据包, 补发状态, 补传图片]
     lottery_term = ['0'] * 15
-    camera_points = []  # 摄像机移动点位 camera_points[[label内存],[区域号],[卫星图坐标]]
-    audio_points = []  # 音效点位 audio_points[[label内存],[区域号],[卫星图坐标]]
-    ai_points = []  # AI点位 ai_points[[label内存],[区域号],[卫星图坐标]]
+    # camera_points = []  # 摄像机移动点位 camera_points[[label内存],[区域号],[卫星图坐标]]
+    # audio_points = []  # 音效点位 audio_points[[label内存],[区域号],[卫星图坐标]]
+    # ai_points = []  # AI点位 ai_points[[label内存],[区域号],[卫星图坐标]]
     map_orbit = []  # 地图轨迹
     previous_channel = None  # 音效通道
     balls_ranking_time = [0] * balls_count  # 每个球的比赛进行时间
@@ -7884,7 +7889,7 @@ if __name__ == '__main__':
         ]
     }
 
-    load_plan_json()
+    # load_plan_json()
 
     positions_live_thread = PositionsLiveThread()  # 发送实时位置到服务器线程
     positions_live_thread.signal.connect(livesignal_accept)
@@ -8102,7 +8107,6 @@ if __name__ == '__main__':
     ui.pushButton_kaj789.clicked.connect(kaj789_table)
 
     "**************************直播大厅_结束*****************************"
-
     deal_udp_thread = DealUdpThread()
     deal_udp_thread.signal.connect(udpsignal_accept)
     deal_udp_thread.start()
