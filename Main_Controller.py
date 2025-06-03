54562dd9e9711ec44e1c5f428eabf96271389ce3
zzw168 <78839407@qq.com>
2025-06-03 16:44:08 +0400

    z

--- Main_Controller.py
+++ Main_Controller.py
@@ -2398,7 +2398,7 @@ class ObsEndThread(QThread):
                 self.signal.emit('比赛计时')
                 continue
             print('结算页面运行！')
-            lottery_term[3] = '已结束'  # 新一期比赛的状态（0.已结束）
+            lottery_term[3] = '已结束'  # 新一期比赛的状态（0.已结束）实时数据停止标志
             Audio_Thread.run_flg = False  # 停止卫星图音效播放线程
             if ui.checkBox_Ai.isChecked():
                 Ai_Thread.run_flg = False  # 停止卫星图AI播放线程
