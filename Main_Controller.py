792c82f17600e88e0645f8987bc0238c22f960ca
zzw168 <78839407@qq.com>
2025-06-16 09:17:41 +0400

    z

--- Main_Controller.py
+++ Main_Controller.py
@@ -815,6 +815,13 @@ def deal_rank(integration_qiu_array):
                                                and 0 < q_item[6] - ranking_check[q_item[5]][0]
                                                < area_limit + balls_count)))
                              )  # 处理最后一圈终点附近的条件
+                             or (q_item[6] > max_area_count - balls_count  # 飞珠提前入港
+                                 and ranking_temp[r_index][6] <= q_item[6] <= max_area_count
+                                 and ranking_temp[0][6] > max_area_count - balls_count
+                                 and (ranking_temp[0][9] >= max_lap_count - 1)
+                                 and (map_label_big.map_action >=
+                                      len(map_label_big.path_points[0]) / 10 * int(ui.lineEdit_Map_Action.text()))
+                                 )
                         )):
                     ranking_check[q_item[5]] = [-1, -1]
                     write_ok = True
